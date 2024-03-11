073df66adc91bb7e52e1eec4bed3813e44d94e67
Martin van Diest <martin.van.diest@intemo.com>
2024-03-11 16:15:29 +0100

    WIP

--- Controller/Api/UserController.php
+++ Controller/Api/UserController.php
@@ -48,7 +48,54 @@ class UserController extends BaseController
      */
     public function checkUserAction()
     {
-        echo("HELLO!");
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        $arrQueryStringParams = $this->getQueryStringParams();
+        if (strtoupper($requestMethod) == 'POST') {
+            try {
+                if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW']))
+                {
+                    $username = $_SERVER['PHP_AUTH_USER'];
+                    $password = $_SERVER['PHP_AUTH_PW'];
+
+                    echo("Username " . $username . " with password " . $password . " received.");
+                    $userModel = new UserModel();
+                    $foundUser = $userModel->getUserCredentials($username);
+                    if(md5($password) == $foundUser['password'])
+                    {
+                        echo("User credentials valid!");
+                    }
+                    else {
+                        $strErrorDesc = $e->getMessage().'User credentials invalid!';
+                        $strErrorHeader = 'HTTP/1.1 404 Invalid Credentials';
+                    }
+                }
+                else
+                {
+                    $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                    $strErrorHeader = 'HTTP/1.1 500 Credentials are needed';
+                }
+
+            } catch (Error $e) {
+                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } else {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+
+        // send output
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } else {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)),
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
 
     }
 }
