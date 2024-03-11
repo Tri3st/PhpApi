edf6f23b232274e80ebed431d8196b81a8f62329
Martin van Diest <martin.van.diest@intemo.com>
2024-03-11 16:02:42 +0100

    Add README.md for extra information

--- Controller/Api/UserController.php
+++ Controller/Api/UserController.php
@@ -48,45 +48,7 @@ class UserController extends BaseController
      */
     protected function checkUserAction()
     {
-        // https://www.php.net/manual/en/features.http-auth.php
-
-        // First check if a username was provided.
-        if (!isset($_SERVER['PHP_AUTH_USER'])) {
-            // If no username provided, present the auth challenge.
-            header('WWW-Authenticate: Basic realm="My Website"');
-            header('HTTP/1.0 401 Unauthorized');
-            // User will be presented with the username/password prompt
-            // If they hit cancel, they will see this access denied message.
-            echo '<p>Access denied. You did not enter a password.</p>';
-            exit; // Be safe and ensure no other content is returned.
-        }
-
-        // If we get here, username was provided. Check password.
-        if ($_SERVER['PHP_AUTH_PW'] == '$ecret') {
-            echo '<p>Access granted. You know the password!</p>';
-        } else {
-            echo '<p>Access denied! You do not know the password.</p>';
-        }
-
-        echo("IN checkUSER ACTION !!! ");
-
-        $strErrorDesc = '';
-        $requestMethod = $_SERVER["REQUEST_METHOD"];
-        $arrQueryStringParams = $this->getQueryStringParams();
-        if (strtoupper($requestMethod) == 'GET') {
-            try {
-                $userModel = new UserModel();
-
-                $arrUsers = $userModel->getUserCredentials($username);
-                $responseData = json_encode($arrUsers);
-            } catch (Error $e) {
-                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
-                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
-            }
-        } else {
-            $strErrorDesc = 'Method not supported';
-            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
-        }
+        echo("HELLO!");
 
     }
 }
