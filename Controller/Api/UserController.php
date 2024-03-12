5dc5dec640dd5606e305ab047d95d8fa31f00b5a
Martin van Diest <martin.van.diest@intemo.com>
2024-03-12 13:28:23 +0100

    WIP

--- Controller/Api/UserController.php
+++ Controller/Api/UserController.php
@@ -69,6 +69,8 @@ class UserController extends BaseController
                     //    string(33) "098f6bcd4621d373cade4e832627b4f6 "
                     //  }
                     //}
+                    echo("The MD5 has of the password is : " . md5($password) . " \n");
+                    echo("And shoudl be : " . md5("test") . " \n")
 
                     if(md5($password) == $passwordFromDb)
                     {
