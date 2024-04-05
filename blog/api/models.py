9c1da8cfeca38a8136665f4543ce14e8b5e1ed88
Martin van Diest <martin.van.diest@gmail.com>
2024-04-05 15:31:43 +0200

    third one

--- blog/api/models.py
+++ blog/api/models.py
@@ -1,4 +1,21 @@
 from django.db import models
 
 # Create your models here.
-CATEGORY_CHOICES = sorted(['', '', '', '', '']
+CATEGORY_CHOICES = sorted(['NEWS', 'INTERESTING', 'PERSONAL', 'WORK', 'TECH', 'OTHER'])
+
+
+def get_category_choices():
+    return {i: i for i in CATEGORY_CHOICES}
+
+
+class Blogpost(models.Model): 
+    title = models.CharField(max_length=100)
+    category = models.CharField(max_length=20, choices=get_category_choices)
+    content = models.TextField()
+    image = models.CharField(max_length=255, blank=True, null=True)
+    author = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
+
--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import Group, User
+from blog.api.models import Blogpost, get_category_choices
 from rest_framework import serializers
 
 
@@ -12,13 +13,18 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         model = Group
         fields = ['url', 'name']
 
-# TODO : finish
-class PostSerializer(serializers.Serializer):
+class BlogPostSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    title = serializer.CharField(required=False, allow_blank=True, max_length=100)
-    category = 
-    content
-    image
-    author
-    created
+    title = serializers.CharField(required=True, max_length=100)
+    category = serializers.ChoiceField(required=True, choices=get_category_choices, default='NEWS')
+    content = serializers.TextField(required=True)
+    image = serializers.CharField(required=False)
+    author = serializers.CharField(required=True)
+    created = serializers.DateTimeField(required=True, auto_now_add=True, read_only=True)
 
+    def create(self, validated_data):
+        return Blogpost.objects.create(**validated_data)
+    
+    class Meta:
+        model = Post
+        fields = ['title', 'category', 'content', 'image', 'author', 'created']
